3a689cc6c2e866fccb09ba458893b8309b1bb744
Andrew Southern <whoamihealay@gmail.com>
2022-03-10 11:25:39 -0500

    Added avatar to last read message

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Box } from '@material-ui/core';
 import { SenderBubble, OtherUserBubble } from '.';
 import moment from 'moment';
@@ -6,13 +6,24 @@ import moment from 'moment';
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
+  const lastMessageRead = useMemo(() => 
+    {
+      let lastIndex = messages.length - 1
+      while (lastIndex !== -1) {
+        if (messages[lastIndex].readStatus === true) {
+        return messages[lastIndex].id
+        }
+        lastIndex--
+      }
+    }, [messages])
+
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format('h:mm');
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble key={message.id} text={message.text} time={time} otherUser={otherUser} messageId={message.id} lastMessageRead={lastMessageRead} />
         ) : (
           <OtherUserBubble
             key={message.id}
