01d5bee3142d94d21ad7bbe7f7014d8bd11d7adc
Andrew Southern <whoamihealay@gmail.com>
2022-03-15 11:29:26 -0400

    removed groupConversation model in favor of a single conversation model with types

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,23 +1,18 @@
 const Conversation = require("./conversation");
-const GroupConversation = require("./groupConversation")
 const User = require("./user");
 const Message = require("./message");
 
 // associations
 
 User.hasMany(Conversation);
-User.hasMany(GroupConversation)
 Conversation.belongsTo(User, { as: "user1" });
 Conversation.belongsTo(User, { as: "user2" });
-GroupConversation.belongsTo(User, {as: "owner"})
-GroupConversation.belongsToMany(User, {through: "groupUsers"})
+Conversation.belongsToMany(User, {through: "groupUsers"})
 Message.belongsTo(Conversation);
-Message.belongsTo(GroupConversation);
 Conversation.hasMany(Message);
 
 module.exports = {
   User,
   Conversation,
-  GroupConversation,
   Message
 };
