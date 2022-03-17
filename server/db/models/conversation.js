51567d66b7f256fda2699548cb607bd56dcc3b85
Andrew Southern <whoamihealay@gmail.com>
2022-03-17 12:40:49 -0400

    moved to a all conversations are group conversations proposal

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -9,6 +9,7 @@ const Conversation = db.define("conversation", {});
 Conversation.findConversation = async function (user1Id, user2Id) {
   const conversation = await Conversation.findOne({
     where: {
+      type: "private",
       user1Id: {
         [Op.or]: [user1Id, user2Id]
       },
@@ -21,6 +22,21 @@ Conversation.findConversation = async function (user1Id, user2Id) {
   // return conversation or null if it doesn't exist
   return conversation;
 };
+
+// find conversation given list of users id
+Conversation.findGroupConversation = async function(senderUser, users) {
+
+  const orderedUsers = [senderUser, ...users].sort((a, b) => (a.id > b.id) ? 1 : -1)
+
+  const conversation = await Conversation.findOne({
+    where: {
+      type: "group",
+      users: orderedUsers
+    }
+  })
+
+  return conversation
+}
 
 Conversation.verifyConversationUsers = async function(conversationId, user1Id, user2Id) {
   const conversation = await Conversation.findOne({
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -5,9 +5,8 @@ const Message = require("./message");
 // associations
 
 User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
-Conversation.belongsToMany(User, {through: "groupUsers"})
+Conversation.belongsTo(User, { as: "owner" });
+Conversation.belongsToMany(User, {through: "conversationUsers"})
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
